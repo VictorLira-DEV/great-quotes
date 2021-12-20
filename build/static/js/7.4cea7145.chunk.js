dc361b6ab2f406695cc397087c858485028c0ef8
VictorLira-DEV <victorliradev@gmail.com>
2021-12-20 16:40:18 -0200

    build production

---
+++ build/static/js/7.4cea7145.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[7],{48:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("div",{children:" Page not Found "})})}}}]);
+//# sourceMappingURL=7.4cea7145.chunk.js.map
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
+import React, { Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import AllQuotes from './pages/allQuotes/AllQuotes';
-import NewQuote from './pages/newQuote/NewQuote';
-import QuoteDetail from './pages/quoteDetail/QuoteDetail';
 import Layoult from './components/layout/layoult/Layout';
-import NotFound from './pages/notFound/NotFound';
+import LoadingSpinner from './components/UI/loadingSpinner/LoadingSpinner';
+
+//Production process
+// lazy loading with  react router
+//lazy loading split out code loading process into chuncks
+//this initial bundle will be smaller
+//irá criar um novo pedaço, que só será baixado quando o usuário visitar
+const NewQuote = React.lazy(() => import('./pages/newQuote/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/quoteDetail/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/notFound/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/allQuotes/AllQuotes'));
 
 function App() {
     return (
         <Layoult>
             {/* <Switch>  v6*/}
-            <Switch>
-                <Route path="/" exact>
-                    <Redirect to="/quotes" />
-                </Route>
+            <Suspense
+                fallback={
+                    <div className="centered">
+                        <LoadingSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/quotes" />
+                    </Route>
 
-                <Route path="/quotes" exact>
-                    <AllQuotes />
-                </Route>
-                {/* this ":quoteId" is the key to access with useParams() */}
-                <Route path="/quotes/:quoteId">
-                    <QuoteDetail />
-                </Route>
-                <Route path="/new-quote">
-                    <NewQuote />
-                </Route>
-                {/* unknown route */}
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
-            {/* </Switch> */}
+                    <Route path="/quotes" exact>
+                        <AllQuotes />
+                    </Route>
+                    {/* this ":quoteId" is the key to access with useParams() */}
+                    <Route path="/quotes/:quoteId">
+                        <QuoteDetail />
+                    </Route>
+                    <Route path="/new-quote">
+                        <NewQuote />
+                    </Route>
+                    {/* unknown route */}
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+                {/* </Switch> */}
+            </Suspense>
         </Layoult>
     );
 }
